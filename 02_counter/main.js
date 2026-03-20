8fe2ba936b6df981a0ea7fdef884037ecd8a84cb
kimjisung <kimjisung4664@gmail.com>
2026-03-20 10:10:04 +0900

    counter HTML onclick

--- 02_counter/main.js
+++ 02_counter/main.js
@@ -6,14 +6,19 @@ const resultH1 = document.getElementById("result");
 
 // button에 있는 + 요소를 가져오자
 // const plusButton = document.getElementsByTagName("button")[0];
-const plusButton = document.getElementsByClassName("plus")[0];
+// const plusButton = document.getElementsByClassName("plus")[0];
 
 // plusButton.addEventListener("click", () => {
 //     number++;
 //     resultH1.innerHTML = number;
 // })
 
-plusButton.onclick = () => {
+// plusButton.onclick = () => {
+//     number++;
+//     resultH1.innerHTML = number;
+// }
+
+const plus = () => {
     number++;
     resultH1.innerHTML = number;
 }
