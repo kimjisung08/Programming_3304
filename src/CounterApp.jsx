db83cc1d2c4388cc38aa639ee57a4ad745e20c24
kimjisung <kimjisung4664@gmail.com>
2026-04-03 09:40:18 +0900

    CounterApp +1, -1, +2, reset

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -11,9 +11,10 @@ function CounterApp() {
   return (
     <>
     <h1>{count}</h1>
-    <button onClick={() => setCount((count) => count + 1)}>+</button>
+    <button onClick={() => setCount((count) => count + 1)}>+1</button>
     <button onClick={() => setCount((count) => count + 2)}>+2</button>
     <button onClick={() => setCount((count) => count - 1)}>-1</button>
+    <button onClick={() => setCount(0)}>reset</button>
 
       {/* <button
         className="counter"
