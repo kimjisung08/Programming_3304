363de6e1e128ae8bf4110019ed6ddd3e88338c92
kimjisung <kimjisung4664@gmail.com>
2026-04-03 09:07:12 +0900

    CounterApp +1

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -2,19 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-// import './App.css'
+// import './App.css' 
 
 function CounterApp() {
+  //count : 현재 또는 이전 상태 값; setCount() : 상태 값 변경 함수
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <button
+    <h1>{count}</h1>
+    <button onClick={() => setCount((count) => count + 1)}>+</button>
+
+      {/* <button
         className="counter"
         onClick={() => setCount((count) => count + 1)}
       >
         Count is {count}
-      </button>
+      </button> */}
     </>
   )
 }
