a2dd8dc1ecc03b00d36e81e86dbcbace2de1813e
kimjisung <kimjisung4664@gmail.com>
2026-04-08 09:34:32 +0900

    TodoListApp 시작 ☝️, HTML

---
+++ src/TodoListApp.jsx
@@ -0,0 +1,23 @@
+function TodoListApp() {
+    return (
+        <div>
+            <h1>No Think, Do It!!</h1>
+
+            <from>
+                <input type="text" />
+                <button>Add</button>
+            </from>
+
+            <ul>
+                <li>
+                    <input type="checkbox" id="chk-1" />
+                    <label htmlFor="chk-1">think</label>
+                    <button>📝</button>
+                    <button>❌</button>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default TodoListApp;
