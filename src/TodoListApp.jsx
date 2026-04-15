0e273b7758a7dda4bf3ce9328bd93c994ed5080b
kimjisung <kimjisung4664@gmail.com>
2026-04-15 10:11:22 +0900

    TodoListApp addTodo() 진행중

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,17 +1,35 @@
+import { useState } from 'react';
 import './todolist.css'
-import TodoItemEmpty from './components/TodoItemEmpty.jsx'
-import Button from './components/Button.jsx'
-import Checkbox from './components/Checkbox.jsx'
+// import TodoItemEmpty from './components/TodoItemEmpty.jsx'
+// import Button from './components/Button.jsx'
+// import Checkbox from './components/Checkbox.jsx'
 import TodoHeader from './components/Todoheader.jsx'
 import TodoAdder from './components/Todoadder.jsx'
-import TodoItem from './components/TodoItem.jsx'
+// import TodoItem from './components/TodoItem.jsx'
 import TodoList from './components/TodoList.jsx'
 
+class Todo {
+    constructor(text) {
+        this.text = text;                 //할일의 내용
+        this.id = Date.now();             //할일 id고유의 값 == new Date().getItem()
+        this.isCompleted = false;         //할일 완료 여부
+    }
+}
+
 function TodoListApp() {
+    const [todos, setTodos] = useState([]);
+    const addTodo = (text) => setTodos((todos) => [
+        //기존 todos 복사
+        ...todos,
+        //newtodo 만들어서
+        //뒤에 추가하자 
+        new Todo(text)
+    ]);
+
     return (
         <div className="todo">
             <TodoHeader/>
-            <TodoAdder/>
+            <TodoAdder addTodo = {addTodo}/>
             <TodoList/>           
         </div>
     )
--- src/components/Todoadder.jsx
+++ src/components/Todoadder.jsx
@@ -1,6 +1,9 @@
 import Button from './Button.jsx'
 
-export default function Todoadder() {
+export default function Todoadder({addTodo}) {
+    //submit이면 nandleSubmit 실행
+    //handleSubmit
+    //Text 받아서 addTodo(Text)
     return (
         <form className="todo_form">
             <input type="text" placeholder="할 일을 입력하세요!" className="todo_input" />
